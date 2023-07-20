69247abe20ec3c27cdb48da5f612b11603cf75fd
gol2580 <gol11971197@gmail.com>
2023-07-20 14:11:55 +0900

    Initial commit + Group,Todo Domain

---
+++ src/main/java/WeMatch/wematch/domain/todo/repository/TodoRepository.java
@@ -0,0 +1,50 @@
+package WeMatch.wematch.domain.todo.repository;
+
+import WeMatch.wematch.domain.todo.entity.Todo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TodoRepository {
+    private static Map<Long, Todo> todolist = new HashMap<>();
+    private static long sequence = 0L;
+
+    private static final TodoRepository instance = new TodoRepository();
+
+    public static TodoRepository getInstance() {
+        return instance;
+    }
+
+    private TodoRepository(){
+
+    }
+
+    public Todo save(Todo todo){
+        todo.setTodoId(++sequence);
+        todolist.put(todo.getTodoId(), todo);
+        return todo;
+    }
+
+    public Todo delete(Todo todo){
+        todolist.remove(todo.getTodoId());
+        return todo;
+    }
+
+//    public Todo findById(Long id){
+//        return todolist.get(id);
+//    }
+
+    public Todo findById(Long TodoId){
+        return todolist.get(TodoId);
+    }
+
+    public List<Todo> findAll() {
+        return new ArrayList<>(todolist.values());
+    }
+
+    public void clearTodolist(){
+        todolist.clear();
+    }
+}
